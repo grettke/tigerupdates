785cb65727a69a7ef9e3c2c2739be403a0080592
Grant Rettke <grant@wisdomandwonder.com>
2022-07-08 15:10:56 -0500

    Link bug

--- tigerupdates.sh
+++ tigerupdates.sh
@@ -130,8 +130,8 @@ cd "$TGRSRC"
 tgrdltxt "https://web.archive.org/web/20141111085423/https://support.apple.com/en-us/HT201259" "Mac OS X: How to verify a SHA-1 digest.html"
 
 cd "$TGRSRC"
-tgrdltxt "https://raw.githubusercontent.com/grettke/tigerupdates/main/tigerupdates.org" "sha1test.org"
-tgrdltxt "https://raw.githubusercontent.com/grettke/tigerupdates/main/tigerupdates.org" "sha1test.sha1"
+tgrdltxt "https://raw.githubusercontent.com/grettke/tigerupdates/main/sha1test.org" "sha1test.org"
+tgrdltxt "https://raw.githubusercontent.com/grettke/tigerupdates/main/sha1test.org" "sha1test.sha1"
 shasum --check sha1test.sha1
 
 cd "$TGRSRC"
